b540124404783cc12a0380c62035b721182cd493
Aung Khaing Khant <2016-miit-cse-004@miit.edu.mm>
2020-11-07 19:23:43 +0630

    Implementation;

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box00 = new System.Windows.Forms.PictureBox();
+            this.box01 = new System.Windows.Forms.PictureBox();
+            this.box10 = new System.Windows.Forms.PictureBox();
+            this.box11 = new System.Windows.Forms.PictureBox();
+            this.box12 = new System.Windows.Forms.PictureBox();
+            this.box02 = new System.Windows.Forms.PictureBox();
+            this.box22 = new System.Windows.Forms.PictureBox();
+            this.box21 = new System.Windows.Forms.PictureBox();
+            this.box20 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.box00)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box20)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // box00
+            // 
+            this.box00.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.box00.Location = new System.Drawing.Point(88, 21);
+            this.box00.Name = "box00";
+            this.box00.Size = new System.Drawing.Size(150, 150);
+            this.box00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.box00.TabIndex = 0;
+            this.box00.TabStop = false;
+            // 
+            // box01
+            // 
+            this.box01.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.box01.Location = new System.Drawing.Point(244, 21);
+            this.box01.Name = "box01";
+            this.box01.Size = new System.Drawing.Size(150, 150);
+            this.box01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.box01.TabIndex = 1;
+            this.box01.TabStop = false;
+            // 
+            // box10
+            // 
+            this.box10.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.box10.Location = new System.Drawing.Point(88, 177);
+            this.box10.Name = "box10";
+            this.box10.Size = new System.Drawing.Size(150, 150);
+            this.box10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.box10.TabIndex = 2;
+            this.box10.TabStop = false;
+            // 
+            // box11
+            // 
+            this.box11.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.box11.Location = new System.Drawing.Point(244, 177);
+            this.box11.Name = "box11";
+            this.box11.Size = new System.Drawing.Size(150, 150);
+            this.box11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.box11.TabIndex = 3;
+            this.box11.TabStop = false;
+            // 
+            // box12
+            // 
+            this.box12.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.box12.Location = new System.Drawing.Point(400, 177);
+            this.box12.Name = "box12";
+            this.box12.Size = new System.Drawing.Size(150, 150);
+            this.box12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.box12.TabIndex = 5;
+            this.box12.TabStop = false;
+            // 
+            // box02
+            // 
+            this.box02.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.box02.Location = new System.Drawing.Point(400, 21);
+            this.box02.Name = "box02";
+            this.box02.Size = new System.Drawing.Size(150, 150);
+            this.box02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.box02.TabIndex = 4;
+            this.box02.TabStop = false;
+            this.box02.Click += new System.EventHandler(this.play);
+            // 
+            // box22
+            // 
+            this.box22.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.box22.Location = new System.Drawing.Point(400, 333);
+            this.box22.Name = "box22";
+            this.box22.Size = new System.Drawing.Size(150, 150);
+            this.box22.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.box22.TabIndex = 8;
+            this.box22.TabStop = false;
+            // 
+            // box21
+            // 
+            this.box21.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.box21.Location = new System.Drawing.Point(244, 333);
+            this.box21.Name = "box21";
+            this.box21.Size = new System.Drawing.Size(150, 150);
+            this.box21.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.box21.TabIndex = 7;
+            this.box21.TabStop = false;
+            // 
+            // box20
+            // 
+            this.box20.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.box20.Location = new System.Drawing.Point(88, 333);
+            this.box20.Name = "box20";
+            this.box20.Size = new System.Drawing.Size(150, 150);
+            this.box20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.box20.TabIndex = 6;
+            this.box20.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(556, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 43);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Reset Board";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(891, 581);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.box22);
+            this.Controls.Add(this.box21);
+            this.Controls.Add(this.box20);
+            this.Controls.Add(this.box12);
+            this.Controls.Add(this.box02);
+            this.Controls.Add(this.box11);
+            this.Controls.Add(this.box10);
+            this.Controls.Add(this.box01);
+            this.Controls.Add(this.box00);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.box00)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box20)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox box00;
+        private System.Windows.Forms.PictureBox box01;
+        private System.Windows.Forms.PictureBox box10;
+        private System.Windows.Forms.PictureBox box11;
+        private System.Windows.Forms.PictureBox box12;
+        private System.Windows.Forms.PictureBox box02;
+        private System.Windows.Forms.PictureBox box22;
+        private System.Windows.Forms.PictureBox box21;
+        private System.Windows.Forms.PictureBox box20;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        static int player = 1;
+        static int turn = 8;
+        int[][] positions = {
+        new int[3],new int[3],new int[3]
+        };
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    positions[i][j] = 0;
+                }
+            }
+            foreach (Control p in this.Controls)
+            {
+                if (p is PictureBox)
+                {
+                    p.Click -= play;
+                    p.Click += play;
+                }
+
+            }
+
+        }
+        public int checkWinner()
+        {
+            int winner = 0;
+            if ((positions[0][0] == positions[1][0]) && positions[0][0] == positions[2][0])
+            {
+                winner = positions[0][0];
+            }else if((positions[0][0] == positions[1][1]) && positions[0][0] == positions[2][2])
+            {
+                winner = positions[0][0];
+            }
+            else if ((positions[0][0] == positions[0][1]) && positions[0][0] == positions[0][2])
+            {
+                winner = positions[0][0];
+            }
+            else if ((positions[0][1] == positions[1][1]) && positions[0][1] == positions[2][1])
+            {
+                winner = positions[0][1];
+            }
+            else if ((positions[0][2] == positions[1][2]) && positions[0][2] == positions[2][2])
+            {
+                winner = positions[0][2];
+            }
+            else if ((positions[1][0] == positions[1][1]) && positions[1][0] == positions[1][2])
+            {
+                winner = positions[1][0];
+            }
+            else if ((positions[2][0] == positions[2][1]) && positions[2][0] == positions[2][2])
+            {
+                winner = positions[2][0];
+            }
+            else if ((positions[0][2] == positions[1][1]) && positions[0][2] == positions[2][0])
+            {
+                winner = positions[2][0];
+            }
+            return winner;
+        }
+        private void play(object sender, EventArgs e)
+        {
+            if (turn != 0)
+            {
+                PictureBox k = (PictureBox)sender;
+            if(k.Image == null)
+            {
+                Char[] str = { k.Name[3], k.Name[4] };
+                int x = Int32.Parse(str[0].ToString());
+                int y = Int32.Parse(str[1].ToString());
+                if (player == 1)
+                {
+                    player = 2;
+                    k.Image = Image.FromFile("x.png");
+                    positions[x][y] = 1;
+                }
+                else if (player == 2)
+                {
+                    player = 1;
+                    k.Image = Image.FromFile("o.jfif");
+                    positions[x][y] = 2;
+                }
+            }
+            int winner = checkWinner();
+            
+                if (winner == 1 || winner == 2)
+                {
+                    MessageBox.Show(String.Format("Player {0} wins!", winner));
+                    winner = 0;
+                    resetBoard();
+                }
+                turn--;
+            }
+            else
+            {
+                MessageBox.Show("DRAW!");
+                resetBoard();
+            }
+
+        }
+        public void resetBoard()
+        {
+            foreach(Control c in this.Controls)
+            {
+                if(c is PictureBox)
+                {
+                    ((PictureBox)c).Image = null;
+                }
+            }
+            foreach (int[] g in positions)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    g[i] = 0;
+                }
+            }
+            turn = 8;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resetBoard();
+        }
+    }
+}
